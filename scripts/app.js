0d2f5cfa0705217eb63ebb0155b906c3b69224d7
EvgeniQwerty <evgeniqwerty1337@gmail.com>
2023-02-19 17:29:03 +0300

    Added some media rules

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,34 @@
+//carousel init start
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 3,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+//carousel init end
+
+//portfolio video and photo start
 const clickables = document.querySelectorAll(".clickable");
 const videoPlayer = document.querySelector(".video-player");
+const carousel = document.querySelector('.swiper');
+let videoOrPhoto = videoPlayer;
 
 function onImgClick(event) {
-    console.log(0);
-  
-    const dataType = event.target.getAttribute("data-type");
+  console.log(0);
+
+  const dataType = event.target.getAttribute("data-type");
   if (dataType === "video") {
+    videoOrPhoto = videoPlayer;
+    
     const url = event.target.getAttribute("data-url");
     videoPlayer.setAttribute("src", url);
     videoPlayer.classList.toggle("display-none");
@@ -19,6 +42,14 @@ function onImgClick(event) {
       console.log(3);
     }
   }
+  else if (dataType === "photo") {
+    
+    videoOrPhoto = carousel;
+    
+    console.log('photo');
+    carousel.classList.toggle("display-none");
+    console.log(carousel.classList.contains('display-none'));
+  }
   console.log(event);
 }
 
@@ -28,17 +59,37 @@ function onDocClick(event) {
   if (
     event.key === "Escape" ||
     (event.type === "click" &&
-      event.target !== videoPlayer &&
-      !event.target.classList.contains('clickable'))
+      event.target !== videoOrPhoto &&
+      !event.target.classList.contains("clickable"))
   ) {
-    videoPlayer.classList.toggle("display-none");
-
-    videoPlayer.setAttribute("src", "");
+    
+    videoOrPhoto.classList.toggle("display-none");
+    if (videoOrPhoto === videoPlayer) {
+      videoOrPhoto.setAttribute("src", "");
+    }
 
     document.removeEventListener("keydown", onDocClick);
     document.removeEventListener("click", onDocClick);
   }
 }
+//portfolio video and photo end
+
+//sorting start
+const projects = document.querySelectorAll('.project');
+
+function onLinkClick(event) {
+  const target = event.target;
+  const sort = target.getAttribute('data-sort');
+
+  if (sort !== 'all') {
+    for (let project of projects) {
+      if (project.getAttribute('data-sort') !== sort) {
+        project.classList.add('display-none');  
+      } 
+    }
+  }
+}
+//sorting end
 
 //main
 for (let img of clickables) {
